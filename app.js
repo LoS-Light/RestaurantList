25892fa201a3e666c43e036aac877d2b8fbac949
LoS-Light <loslight1994@gmail.com>
2023-06-29 04:47:07 +0800

    prod: Distribution to deta space

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import cors from 'cors';
 import { MainRouter } from './routers/main.router.js';
 
-const port = 3000;
-const prefix = "/v1";
-const configCors = { origin: ['http://localhost:5500', 'http://127.0.0.1:5500'] }    // Dev only Domain
+const port = process.env.PORT || 3000;
+const prefix = '/v1';
+const staticRes = 'web';
 
 const app = express();
-app.use(cors(configCors));        // Simulate slow network environment
-app.use(prefix, MainRouter);    // Set main router with global prefix
+app.use(express.static(staticRes));     // My web site
+app.use(prefix, MainRouter);        // Set api main router with global prefix
 
 app.listen(port, () => console.log(`-> Listening on ${port}`));
--- web/scripts/const.js
+++ web/scripts/const.js
@@ -1,1 +1,16 @@
-export const API_ROOT = `http://localhost:3000/v1`;
+const DEV_API = 'http://localhost:3000/v1';
+const PROD_API = 'https://restaurantlist.loslight.com/v1';
+
+function getApi() {
+    const hostname = location.hostname;
+    if (hostname === 'localhost') {
+        console.log('-> Environment: Dev');
+        return DEV_API;
+    }
+    else {
+        console.log('-> Environment: Prod');
+        return PROD_API;
+    }
+}
+
+export const API_ROOT = getApi();
